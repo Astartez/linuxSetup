a25ba93756000e9c393097f268f8e5a7a9f38799
Astartez <aagaard358@gmail.com>
2022-06-28 18:55:12 -0300

    Update mic mute script to all pulseaudio sources

--- scripts/audio_input_mute_toggle.sh
+++ scripts/audio_input_mute_toggle.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-if [ $( amixer set Capture toggle | awk '/Front Left:/{ print $NF }' ) == "[on]" ]; then
-    notify-send "Microphone: Unmuted" --icon=audio-input-microphone
-else
-    notify-send "Microphone: Muted" --icon=audio-input-microphone
-fi
+# Mute state for default source: yes/no
+default_source_mute_state=$(pactl get-source-mute $(pactl get-default-source) | awk '{ print $2 }')
 
+source_name_list=$( pactl list short sources | awk '!/.monitor/{ print $2 }' )
+
+if [[ $default_source_mute_state = "no" ]]; then
+    # I MUST MUTE
+    for source_name in $source_name_list;do
+        pactl set-source-mute $source_name 1
+    done
+    notify-send "Microphone: Muted" --icon=audio-input-microphone
+else
+    # I MUST UNMUTE
+    for source_name in $source_name_list;do
+        pactl set-source-mute $source_name 0
+    done
+    notify-send "Microphone: Unmuted" --icon=audio-input-microphone
+fi
