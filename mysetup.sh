a58b508d4af10ff3d2f13c61196e549e78da8980
Astartez <26726942+Astartez@users.noreply.github.com>
2022-10-19 00:10:26 -0300

    Add macOS support

--- mysetup.sh
+++ mysetup.sh
@@ -50,26 +50,36 @@ function check_if_install_no {
 # VARIABLES
 # ========================================
 
-base_dir=$(dirname $(realpath $0))
+base_dir=$(cd "$(dirname "$0")"; pwd -P) 
+machine=$(uname -s)
 
 # ========================================
 # RUN
 # ========================================
 
 if check_if_install_yes "fish"; then
-    check_dependency curl
-    check_dependency gpg
-    check_dependency apt-get
-    if [[ $(cat /etc/debian_version) == "10"* ]]; then # debian 10
-        echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
-        curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-    elif [[ $(cat /etc/debian_version) == "11"* ]]; then # debian 11
-        echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
-        curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-    else # ubuntu
+    if [[ $machine == "Linux" ]]; then
+        check_dependency apt-get
+        if [[ $(cat /etc/debian_version) == "10"* ]]; then # debian 10
+            check_dependency curl
+            check_dependency gpg
+            echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+            curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+        elif [[ $(cat /etc/debian_version) == "11"* ]]; then # debian 11
+            check_dependency curl
+            check_dependency gpg
+            echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+            curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+        else # ubuntu
             sudo apt-add-repository ppa:fish-shell/release-3
+        fi
+        sudo apt-get update && sudo apt-get install --no-install-recommends --yes fish
+    elif [[ $machine == "Darwin" ]]; then # macOS
+        check_dependency brew
+        brew install fish
+    else
+        echo "Not linux or macOS. Aborting." && exit 1
     fi
-    sudo apt-get update && sudo apt-get install --no-install-recommends --yes fish
 fi
 
 if check_if_install_yes "fisher + pure theme + colored man pages + fish colors"; then
@@ -96,23 +106,48 @@ if check_if_install_yes "fzf"; then
 fi
 
 if check_if_install_yes "micro"; then
-    check_dependency curl
-    check_dependency apt-get
-    curl https://getmic.ro | sudo bash
-    sudo mv micro /usr/bin
-    # If X server is running install xclip
-    if timeout 1s xset q &>/dev/null; then
-        sudo apt-get update && sudo apt-get install --no-install-recommends --yes xclip
+    if [[ $machine == "Linux" ]]; then
+        check_dependency curl
+        curl https://getmic.ro | sudo bash
+        sudo mv micro /usr/bin
+        # If X server is running install xclip
+        if timeout 1s xset q &>/dev/null; then
+            check_dependency apt-get
+            sudo apt-get update && sudo apt-get install --no-install-recommends --yes xclip
+        fi
+    elif [[ $machine == "Darwin" ]]; then # macOS
+        check_dependency brew
+        arch -arm64 brew install micro
+    else
+        echo "Not linux or macOS. Aborting." && exit 1
     fi
 fi
 
 if check_if_install_yes "grc and fish functions (ll;la;apt)"; then
-    check_dependency apt-get
-    sudo apt-get update && sudo apt-get install --no-install-recommends --yes grc
-    fish -c "fisher install orefalo/grc"
-    sudo sed -i 's/ls -lh $argv/ls -lh --group-directories-first $argv/g' /usr/share/fish/functions/ll.fish
-    sudo sed -i 's/ls -lAh $argv/ls -lAh --group-directories-first $argv/g' /usr/share/fish/functions/la.fish
-    cp $base_dir/config_files/fish_functions/apt-update-upgrade-autoremove-clean.fish ~/.config/fish/functions/
+    check_dependency fish
+    rm ~/.config/fish/functions/ll.fish
+    rm ~/.config/fish/functions/la.fish
+    cp $(fish -c "type -p ll") ~/.config/fish/functions/
+    cp $(fish -c "type -p la") ~/.config/fish/functions/
+    if [[ $machine == "Linux" ]]; then
+        check_dependency apt-get
+        sudo apt-get update && sudo apt-get install --no-install-recommends --yes grc
+        cp $base_dir/config_files/fish_functions/apt-update-upgrade-autoremove-clean.fish ~/.config/fish/functions/
+        fish -c "fisher install orefalo/grc"
+        sed -i 's/ls -lh $argv/ls -lh --group-directories-first $argv/g' ~/.config/fish/functions/ll.fish
+        sed -i 's/ls -lAh $argv/ls -lAh --group-directories-first $argv/g' ~/.config/fish/functions/la.fish
+    elif [[ $machine == "Darwin" ]]; then # macOS
+        check_dependency brew
+        arch -arm64 brew install grc
+        brew install coreutils
+        arch -arm64 brew install gnu-sed
+        fish -c "fisher install usami-k/gls"
+        fish -c "fisher install orefalo/grc"
+        gsed -i 's/ls -lh $argv/gls -lh --group-directories-first $argv/g' ~/.config/fish/functions/ll.fish
+        gsed -i 's/ls -lAh $argv/gls -lAh --group-directories-first $argv/g' ~/.config/fish/functions/la.fish
+    else
+        echo "Not linux or macOS. Aborting." && exit 1
+    fi
 fi
 
 if check_if_install_no "audio scripts + libnotify"; then
